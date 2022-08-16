fca8c21009fd6df429a9d836cf2cd6af98c37784
wanglei <wanglei_gmgc@hotmail.com>
2022-08-16 23:34:03 +0800

    add detailed notes to Chapter6.

--- apue-src/006/6_10/6_10_155.c
+++ apue-src/006/6_10/6_10_155.c
@@ -5,13 +5,22 @@
 int
 main(void)
 {
+	// time_t用于记录从1970年1月1日00:00:00（格林威治时间）至今所经过的秒数，
+	// 我们称之为日历时间。
 	time_t			t;
+	// 一个专门用于描述时间、星期和年月日等详细信息的结构体。
 	struct tm		*tmp;
+	// 下面两个缓冲区给strftime()函数使用。
 	char			buf1[16];
 	char			buf2[64];
 
+	// 计算格林威治时间至今所经过的秒数，存储为日历时间。
 	time(&t);
+	// localtime()函数将日历时间转换为本地时间(考虑到本地时区和夏令时标志)。
+	// 将转换后的时间存储到tm结构体，将其指针返回。
 	tmp = localtime(&t);
+	// strftime()函数是时间格式化输出函数，它可以把tm时间以任意我们给定的格式进行输出。
+	// 我们使用了两个不同大小的缓冲区，为了演示16个字节的小缓冲区无法容纳输出信息。
 	if (strftime(buf1, 16, "time and date: %r, %a %b %d, %Y", tmp) == 0)
 		printf("buffer length 16 is too small\n");
 	else
@@ -22,3 +31,8 @@ main(void)
 		printf("%s\n", buf2);
 	exit(0);
 }
+
+// 扩展
+// 1.有一个和localtime()相类似的函数gmtime()，该函数会将日历时间转换为UTC(协调统一时间)
+// 的年、月、日、时、分、秒、周日分解结构。
+// 2.strptime()函数是strftime()函数反过来的版本，它可以把字符串时间转换为分解时间（tm结构体）。
--- apue-src/006/6_2/6_2_144.c
+++ apue-src/006/6_2/6_2_144.c
@@ -3,32 +3,51 @@
 #include <string.h>
 #include "apue.h"
 
+// 本例给出了getpwnam()的一个实现。
+// getpwnam()函数通过登录名获取其在/etc/passwd文件中的详细信息，该函数被login程序使用。
 struct passwd *
 getpwnam(const char *name)
 {
+	// 密码文件结构体指针
 	struct passwd	*ptr;
 
+	// setpwent()用来将getpwent()的读写地址指回密码文件开头。
+	// 这里起着保护作用，如果之前用户已经调用了getpwent()，这里的调用将确保读写地址定位到密码文件开头。
 	setpwent();
+	// 调用getpwent()函数遍历读取密码文件。
 	while ((ptr = getpwent()) != NULL)
+		// 找到和目标登录名一致的条目后跳出循环。
 		if (strcmp(name, ptr->pw_name) == 0)
 			break;		/* found a match */
+	// getpwent()和setpwent()函数会打开passwd等相关文件，查询结束后必须调用endpwent()函数关闭这些文件。
 	endpwent();
+	// 将读到的信息指针返回给用户，如果未找到信息，则返回NULL。
 	return (ptr);		/* ptr is NULL if no match found */
 }
 
 // Add by Richard Wang
 // 2021-12-07
+// 测试程序
 int main(int argc, char **argv) {
-	if (argc < 2) {
+	// 仅接受1个参数，参数为登录名。
+	if (argc != 2) {
 		printf("Usage: exc <user_name>\n");
 		exit(0);
 	}
+	// 调用我们自己实现的getpwnam()函数查询密码结构信息。
 	struct passwd *pwd = getpwnam(argv[1]);
-	if (NULL == pwd)
+	// 未查到信息。
+	if (NULL == pwd) {
 		err_ret("getpwnam error");
+		exit(0);
+	}
+	// 查到信息后输出查询结果。
 	printf("name:%s, pass:%s, uid:%u, gid:%u, rname:%s, dir:%s, shell:%s\n",
 		pwd->pw_name, pwd->pw_passwd, pwd->pw_uid, pwd->pw_gid,
 		pwd->pw_gecos, pwd->pw_dir, pwd->pw_shell);
 	
 	exit(0);
 }
+
+// 扩展
+// 还有一个函数根据uid来查询passwd的详细信息，这个函数是getpwuid(uid_t uid)。
